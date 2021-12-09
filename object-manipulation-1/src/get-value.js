128916ed826554998efb2d4f683d2e8ff32ec1f2
Lindsey Laveirge <85596835+llaveirge@users.noreply.github.com>
2021-12-09 16:14:40 -0700

    Completed object-manipulation-1 challenge. -Lindsey Laveirge

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,9 @@
+/* exported getValue */
+
+// Define a function called 'getValue' which has a single parameter: 'object'
+// Locate the 'key' parameter property of the object
+// Return the property value
+
+function getValue(object, key) {
+  return object[key];
+}
---
+++ object-manipulation-1/src/set-value.js
@@ -0,0 +1,10 @@
+/* exported setValue */
+
+// Define the function 'setValue' with three parameters: 'object', 'key', and 'value'
+// Assign the 'value' parameter as the value for the 'key' parameter in the 'object' parameter
+// Use bracket notation to access a property in an object in order to assign it a value
+// Do not return anything from the function
+
+function setValue(object, key, value) {
+  object[key] = value;
+}
